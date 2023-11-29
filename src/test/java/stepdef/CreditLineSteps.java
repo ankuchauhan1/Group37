ae84c051afacc887182364392557a3642d741da1
salad <sarita.lad@fiserv.com>
2023-11-29 12:46:27 +0530

    Credit line

--- src/test/java/stepdef/CreditLineSteps.java
+++ src/test/java/stepdef/CreditLineSteps.java
@@ -1,17 +1,25 @@
 package stepdef;
 
-import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.sdet.CreditLinePage;
-import org.sdet.LoginApplication;
-import org.sdet.SelectBank;
-import org.utils.TestConfig;
 
 public class CreditLineSteps {
 
     CreditLinePage CreditLine = new CreditLinePage();
+
     @When("User navigates to Credit Line screen")
-    public void move_to_CreditLine_page(){
+    public void move_to_CreditLine_page()
+    {
         CreditLine.moveToCreditLinePage();
     }
+
+
+    @Then("Input all required fields on the Credit Line screen")
+    public void fill_all_the_details(){
+        CreditLine.Fill_Details();
+    }
+
+
+
 }
