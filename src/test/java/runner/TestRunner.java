d053f1d76a6ea2236198bfc26e5a3e44365f20e3
salad <sarita.lad@fiserv.com>
2023-12-01 17:30:06 +0530

    credit line4

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -2,10 +2,7 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-//import org.junit.AfterClass;
-//import org.junit.runner.RunWith;
-//import org.testng.annotations.AfterClass;
-//import org.testng.annotations.Test;
+
 
 //@RunWith(Cucumber.class)
     @CucumberOptions(features= {"src/test/FeatureFiles"},
